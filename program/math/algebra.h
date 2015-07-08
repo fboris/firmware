4f2bc2bede3f347404a46e5549840c0933f53c98
fboris <fantasyboris@gmail.com>
2015-07-08 23:39:57 +0800

    Complete some basic matrix operations and tests TODO: ARM DSP Optimization and other functions

--- program/math/algebra.h
+++ program/math/algebra.h
@@ -62,4 +62,145 @@ static inline vect3 vect3_normalized(const vect3 *a)
 	vect3 v = {a->x * u, a->y * u, a->z * u};
 	return v;
 }
+
+/*
+A 3x3 matrix type
+it is row-major order
+*/
+typedef struct mat33{
+	union{
+		float m[9];
+
+		struct {
+			float m00, m01, m02,
+			      m10, m11, m12,
+			      m20, m21, m22;
+		};
+	};
+}mat33;
+
+/*
+initialize matrix
+*/
+static inline mat33 mat33_init( float m00, float m01, float m02,
+				float m10, float m11, float m12,
+				float m20, float m21, float m22)
+{
+	mat33 m = { m00, m01, m02,
+		    m10, m11, m12,
+	            m20, m21, m22};
+
+	return m;
+}
+
+/*
+
+3x3 identity matrix
+  [ 1 0 0;
+    0 1 0;
+    0 0 1;]
+
+*/
+
+static inline mat33 mat33_identity()
+{
+	mat33 m = { 1.0f, 0.0f, 0.0f,
+		    0.0f, 1.0f, 0.0f,
+	            0.0f, 0.0f, 1.0f};
+	return m;
+}
+
+/*
+
+3x3 diagonal matrix
+  [ m00  0   0;
+    0    m11 0;
+    0    0   m22;]
+
+*/
+
+static inline mat33 mat33_diag(float m00, float m11, float m22)
+{
+	mat33 m = { m00, 0.0f, 0.0f,
+		    0.0f, m11, 0.0f,
+	            0.0f, 0.0f, m22};
+
+	return m;
+}
+
+/*
+matrix addition
+m_out = m1 + m2
+*/
+
+static inline void mat33_add(mat33 *m_out, const mat33 *m1, const mat33 *m2)
+{
+	m_out->m00 = m1->m00 + m2->m00;
+	m_out->m01 = m1->m01 + m2->m01;
+	m_out->m02 = m1->m02 + m2->m02;
+	m_out->m10 = m1->m10 + m2->m10;
+	m_out->m11 = m1->m11 + m2->m11;
+	m_out->m12 = m1->m12 + m2->m12;
+	m_out->m20 = m1->m20 + m2->m20;
+	m_out->m21 = m1->m21 + m2->m21;
+	m_out->m22 = m1->m22 + m2->m22;
+
+}
+
+
+/*
+matrix subtraction
+m_out = m1 - m2
+*/
+
+static inline void mat33_sub(mat33 *m_out, const mat33 *m1, const mat33 *m2)
+{
+	m_out->m00 = m1->m00 - m2->m00;
+	m_out->m01 = m1->m01 - m2->m01;
+	m_out->m02 = m1->m02 - m2->m02;
+	m_out->m10 = m1->m10 - m2->m10;
+	m_out->m11 = m1->m11 - m2->m11;
+	m_out->m12 = m1->m12 - m2->m12;
+	m_out->m20 = m1->m20 - m2->m20;
+	m_out->m21 = m1->m21 - m2->m21;
+	m_out->m22 = m1->m22 - m2->m22;
+
+}
+
+/*
+matrix multiplication
+m_out = m1*m2
+*/
+
+static inline void mat33_multiply(mat33 *m_out, const mat33 *m1, const mat33 *m2)
+{
+	m_out->m00 = m1->m00 * m2->m00 + m1->m01 * m2->m10 + m1->m02 * m2->m20;
+	m_out->m01 = m1->m00 * m2->m01 + m1->m01 * m2->m11 + m1->m02 * m2->m21;
+	m_out->m02 = m1->m00 * m2->m02 + m1->m01 * m2->m12 + m1->m02 * m2->m22;
+	m_out->m10 = m1->m10 * m2->m00 + m1->m11 * m2->m10 + m1->m12 * m2->m20;
+	m_out->m11 = m1->m10 * m2->m01 + m1->m11 * m2->m11 + m1->m12 * m2->m21;
+	m_out->m12 = m1->m10 * m2->m02 + m1->m11 * m2->m12 + m1->m12 * m2->m22;
+	m_out->m20 = m1->m20 * m2->m00 + m1->m21 * m2->m10 + m1->m22 * m2->m20;
+	m_out->m21 = m1->m20 * m2->m01 + m1->m21 * m2->m11 + m1->m22 * m2->m21;
+	m_out->m22 = m1->m20 * m2->m02 + m1->m21 * m2->m12 + m1->m22 * m2->m22;
+}
+
+/*
+matrix transposed
+m_out = m_in'
+*/
+
+static inline void mat33_transpose(mat33 *m_out, const mat33 *m_in)
+{
+	m_out->m[0] = m_in->m[0];
+	m_out->m[1] = m_in->m[3];
+	m_out->m[2] = m_in->m[6];
+	m_out->m[3] = m_in->m[1];
+	m_out->m[4] = m_in->m[4];
+	m_out->m[5] = m_in->m[7];
+	m_out->m[6] = m_in->m[2];
+	m_out->m[7] = m_in->m[5];
+	m_out->m[8] = m_in->m[8];
+}
+
 #endif
--- program/math/test_algebra.c
+++ program/math/test_algebra.c
@@ -4,6 +4,9 @@
 
 #define DEBUG_PRINT printf
 #define DISPLAY_VECT3(_v) DEBUG_PRINT("[%f %f %f]'", _v.x, _v.y, _v.z)
+#define DISPLAY_MAT33(_m) DEBUG_PRINT("[%f %f %f\r\n%f %f %f\r\n%f %f %f]\r\n", _m.m00, _m.m01, _m.m02, \
+									    _m.m10, _m.m11, _m.m12, \
+									    _m.m20, _m.m21, _m.m22)
 
 int main()
 {
@@ -47,6 +50,40 @@ int main()
 	DEBUG_PRINT("normalized v1 = ");
 	DISPLAY_VECT3(v3);
 	DEBUG_PRINT("\r\n ");
+
+	mat33 m = mat33_init(1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f, 1.8f, 1.9f);
+	DEBUG_PRINT("show m:\r\n");
+	DISPLAY_MAT33(m);
+	DEBUG_PRINT("\r\n");
+
+	m = mat33_identity();
+	DEBUG_PRINT("show 3X3 identity matrix:\r\n");
+	DISPLAY_MAT33(m);
+	DEBUG_PRINT("\r\n");
+
+	m = mat33_diag(1.4f, 5.5f, 6.6f);
+	DEBUG_PRINT("show 3X3 diagonal matrix:\r\n");
+	DISPLAY_MAT33(m);
+
+	mat33 m1 = mat33_init(1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f, 1.8f, 1.9f);
+	mat33 m2 = mat33_init(2.1f, 2.2f, 2.3f, 2.4f, 2.5f, 2.6f, 2.7f, 2.8f, 2.9f);
+	mat33 m_out;
+
+	mat33_add( &m_out, &m1, &m2);
+	DEBUG_PRINT( "show m1 + m2:\r\n");
+	DISPLAY_MAT33( m1);
+	DEBUG_PRINT( "+\r\n");
+	DISPLAY_MAT33( m2);
+	DEBUG_PRINT( "=");
+	DISPLAY_MAT33( m_out);
+
+	mat33_multiply( &m_out, &m1, &m2);
+	DEBUG_PRINT("m1*m2 = \r\n");
+	DISPLAY_MAT33( m_out);
+
+	mat33_transpose( &m_out, &m1);
+	DEBUG_PRINT("m1' = \r\n");
+	DISPLAY_MAT33( m_out);
 	return 0;
 
 }
